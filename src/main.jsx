4949ab811b0f992a226fc7e5614aa0322c8d9316
Bryan De La Cruz <bryan.delacruza@gmail.com>
2023-05-14 12:58:59 -0500

    cambio de ruta de componente importado

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./main.css";
-import { LoginPage } from "./pages/loginPage.jsx";
+import { LoginPage } from "./pages/LoginPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoginPage />
+    <LoginPage></LoginPage>
   </React.StrictMode>
 );
